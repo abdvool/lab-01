33b2cb70cbf66140b709ca25ab680925ab6a19c6
abdvool <abdvool@gmail.com>
2021-06-21 13:51:52 +0300

    edit

---
+++ js/app.js
@@ -0,0 +1,55 @@
+// var noob
+
+// var userName1 = prompt("what is your name")
+// console.log(userName1);
+
+
+// var userName = prompt("how old are you?")
+// console.log(userName);
+
+
+// var userName = prompt("what is your favorite game? ")
+// console.log(userName);
+
+// var userName2 = prompt("Are you noob? ")
+
+// console.log(userName2);
+
+
+
+
+
+
+
+var name = prompt("what is your name?")
+
+alert("welcome")
+
+var pro = "yes"
+
+var pro = prompt("Are you pro?")
+
+if ( pro == "yes"){
+
+    alert("Ma nigga");} else {
+
+        alert("go out of here noob ");
+
+         
+let wew = 'You are';
+let ez = 'Noob';
+console.log(wew + ez);
+
+    }
+
+
+var game = prompt("what is your favorite game? ")
+
+alert("Nice")
+
+console.log(game);
+
+
+prompt("Sumbbit your review please")
+
+alert("Thank your for your time")
